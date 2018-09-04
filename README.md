f731e470c121baaf399e042de2c5729947016666
Ruth Naebeck <ruthnaebeck@gmail.com>
2018-09-03 20:05:24 -0400

    Add main and secrets example, Update README

--- README.md
+++ README.md
@@ -5,19 +5,23 @@ Used to add events to hearthstonecalendar.com
 
 ## Windows Install Guide
 
-Prereq - Have Chrome Installed
+- Prereq - Have Chrome Installed
+- Install Python 3 (comes with pip): https://www.python.org/downloads/
+- `pip install selenium`
+- Install chocolatey: https://chocolatey.org/install
+- `choco install chromedriver`
+- Restart cmd and IDLE (if running)
+- Try the test python script (replace Firefox with Chrome): https://www.seleniumhq.org/docs/03_webdriver.jsp#introducing-the-selenium-webdriver-api-by-example
 
-Install Python 3 (comes with pip)
-https://www.python.org/downloads/
+## Create an events.csv file
+The file needs to be comma delimited (or edit main.py) with the following on each line:
+- Event Title
+- Region (currently setup to work with Americas and Europe)
+- Event Date
+- Event Time (24 hour format + use your set timezone on the site)
+- Link Text (such as "Tournament Registration")
+- Link URL
+- Body message (currently setup to accept HTML)
 
-`pip install selenium`
-
-Install chocolatey
-https://chocolatey.org/install
-
-`choco install chromedriver`
-
-Restart cmd and IDLE (if running)
-
-Try the test python script (replace Firefox with Chrome):
-https://www.seleniumhq.org/docs/03_webdriver.jsp#introducing-the-selenium-webdriver-api-by-example
+## Run it
+`python main.py`
---
+++ main.py
@@ -0,0 +1,74 @@
+import csv
+import secrets
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
+from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+
+# Create a new instance of the driver
+driver = webdriver.Chrome()
+
+# Go to Events Add Page
+driver.get('http://hearthstonecalendar.com/events/add')
+
+# Login
+userName = driver.find_element_by_name('name')
+passWord = driver.find_element_by_name('pass')
+userName.send_keys(secrets.userName)
+passWord.send_keys(secrets.passWord)
+passWord.submit()
+
+def fillForm(nextEvt):
+    # Fill out the form
+    eventName.send_keys(nextEvt[0].replace('ï»¿', ''))
+    # Marks the event as Open Registration
+    openRegistr.click()
+    if nextEvt[1] == 'Americas':
+        regionAM.click()
+    else:
+        regionEU.click()
+    dateEl.clear()
+    dateEl.send_keys(nextEvt[2])
+    timeEl.send_keys(nextEvt[3])
+    linkTitle.send_keys(nextEvt[4])
+    linkUrl.send_keys(nextEvt[5])
+    # Click the source button to add HTML to the message body
+    sourceBtn.click()
+    textBox = driver.find_element_by_css_selector('textarea.cke_source')
+    textBox.send_keys(nextEvt[6])
+    print(nextEvt[0])
+
+with open('events.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+
+    for row in reader:
+        try:
+            WebDriverWait(driver, 15).until(EC.title_contains('Create'))
+            print(driver.title)
+
+            # Define form elements
+            eventName = driver.find_element_by_id('edit-title')
+            openRegistr = driver.find_element_by_id('edit-field-open-registration-und')
+            regionAM = driver.find_element_by_id('edit-field-region-und-15')
+            regionEU = driver.find_element_by_id('edit-field-region-und-16')
+            dateEl = driver.find_element_by_id('edit-field-start-time-und-0-value-datepicker-popup-0')
+            timeEl = driver.find_element_by_id('edit-field-start-time-und-0-value-timeEntry-popup-1')
+            linkTitle = driver.find_element_by_id('edit-field-links-und-0-title')
+            linkUrl = driver.find_element_by_id('edit-field-links-und-0-url')
+            saveBtn = driver.find_element_by_id('edit-submit')
+            sourceBtn = driver.find_element_by_id('cke_67')
+            
+            fillForm(row)
+            saveBtn.click()
+            print('Event added normally!')
+
+        except NoSuchElementException:
+            WebDriverWait(driver, 10)
+            fillForm(next(reader))
+            saveBtn.click()
+            print('Event added after waiting longer!')
+            
+        WebDriverWait(driver, 15).until(EC.title_contains(secrets.eventKeyword))
+        print(driver.title)
+        print('-----------------------')
+        driver.get('http://hearthstonecalendar.com/events/add')
